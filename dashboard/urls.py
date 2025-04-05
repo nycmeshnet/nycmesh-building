7654f67c2402c0182d72c5104faa1bbf7c334ae7
Daniel Heredia <danielhmetro@gmail.com>
2025-04-04 23:20:48 -0400

    Add OIDC to urls.py and create GSG submenu

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from . import views
+from . import views-gsg
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("reports", views.reports, name="reports"),
-    path("billing", views.billing, name="billing")
+    path("gsg/", views-gsg.index, name="index"),
+    path("gsg/reports", views-gsg.reports, name="reports"),
+    path("gsg/billing", views-gsg.billing, name="billing"),
+    path('oidc/', include('mozilla_django_oidc.urls')),
     #path("install/<uuid:pk>/", views.install, name="install")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
