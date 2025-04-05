bcf9d99672a5e20efd0b07334d04f72a4093a8af
Daniel Heredia <danielhmetro@gmail.com>
2025-04-05 00:38:39 -0400

    Fix views_gsg module name in urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 from . import views_gsg
 
 urlpatterns = [
-    path("gsg/", views-gsg.index, name="index"),
-    path("gsg/reports", views-gsg.reports, name="reports"),
-    path("gsg/billing", views-gsg.billing, name="billing"),
+    path("gsg/", views_gsg.index, name="index"),
+    path("gsg/reports", views_gsg.reports, name="reports"),
+    path("gsg/billing", views_gsg.billing, name="billing"),
     path('oidc/', include('mozilla_django_oidc.urls')),
     #path("install/<uuid:pk>/", views.install, name="install")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
