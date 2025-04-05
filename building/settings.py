f50107b299d7329e6586348a29a1d85ed5562d9b
Daniel Heredia <danielhmetro@gmail.com>
2025-04-05 00:57:16 -0400

    Move sessions before auth middlewares settings.py

--- building/settings.py
+++ building/settings.py
@@ -46,8 +46,8 @@ AUTHENTICATION_BACKENDS = (
 )
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
