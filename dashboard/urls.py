f1cbb1eef9a757c0bd00c450c27c898af71bbcda
Daniel Heredia <danielhmetro@gmail.com>
2025-04-05 01:20:08 -0400

    Add root path to urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,12 +2,14 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
 from . import views_gsg
 
 urlpatterns = [
-    path("gsg/", views_gsg.index, name="index"),
-    path("gsg/reports", views_gsg.reports, name="reports"),
-    path("gsg/billing", views_gsg.billing, name="billing"),
+    path('/', views.index, name='index'),
+    path("gsg/", views_gsg.index, name="gsg_index"),
+    path("gsg/reports", views_gsg.reports, name="gsg_reports"),
+    path("gsg/billing", views_gsg.billing, name="gsg_billing"),
     path('oidc/', include('mozilla_django_oidc.urls')),
     #path("install/<uuid:pk>/", views.install, name="install")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
