c6f62e49edc3006b0845e1dd73873663aac55822
Daniel Heredia <danielhmetro@gmail.com>
2025-10-12 23:21:26 -0400

    Revert alternate unit display in both GSG and PH (#100)
    * Revert alternate unit display in ph-index.html
    
    * Fix getting of unit with member id in views_gsg.py

--- dashboard/views_gsg.py
+++ dashboard/views_gsg.py
@@ -378,6 +378,7 @@ def index(request):
         response = requests.get(f"{MEMBER_API_URL}/{member_id}", headers=headers)
         if response.status_code == 200:
             selected_member_info = response.json()
+            selected_member_info['unit'], nn = get_install_unit_and_network_number(selected_member_info['installs'][0]['id'], headers)
             subscription_info = fetch_subscription_info(selected_member_info)
 
     return render(request, 'dashboard/gsg-index.html', {
